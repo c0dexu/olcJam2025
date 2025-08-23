8c22f03b0fcef62b08658e285cb564b266415d55
imserge <sergiu.smw@gmail.com>
2025-08-23 21:58:14 +0300

    typo

--- main.js
+++ main.js
@@ -3,6 +3,4 @@ import * as THREE from "three";
 const canvasContainer = document.getElementsByClassName("canvas-container")[0];
 const renderer = new THREE.WebGLRenderer();
 canvasContainer.appendChild(renderer.domElement);
-renderer.setAnimationLoop(animate);
 renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientWidth);
-renderer.setAnimationLoop(animate);
