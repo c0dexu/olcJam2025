3485318a891c9ae4a7f156d73663f425e4f006c8
imserge <sergiu.smw@gmail.com>
2025-08-23 23:01:13 +0300

    added entity mover

--- src/entities/entity.js
+++ src/entities/entity.js
@@ -1,12 +1,14 @@
+import { hashArray } from "three/src/nodes/core/NodeUtils.js";
 import { MeshBody } from "../meshbody";
 
+// entity should only hold position and mesh info
 export class Entity {
-  x0;
-  y0;
-  z0;
   meshBody;
   geometry_type;
   mesh;
+  x0;
+  y0;
+  z0;
 
   constructor(x0, y0, z0, geometry_type, texture_path, color, args) {
     this.meshBody = new MeshBody(
@@ -20,5 +22,9 @@ export class Entity {
     );
     this.meshBody.initMesh();
     this.mesh = this.meshBody.mesh;
+  }
+
+  addToScene(scene) {
+    scene.add(this.mesh);
   }
 }
---
+++ src/entities/entitymover.js
@@ -0,0 +1,4 @@
+import { Entity } from "./entity";
+
+// TODO: entity used for objects affected by physics
+export class EntityMover extends Entity {}
